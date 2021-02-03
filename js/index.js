48e80916e36b10ec6780c8cc11cc6133f88c5972
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2021-02-03 02:07:41 +0000

    Basic working one channel input

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const CHANNELS = "rgba";
+const redInputCanvas = document.getElementById("red-canvas");
 
 addInputListeners(document.getElementById("red-file"), document.getElementById("red-canvas"));
 
@@ -13,9 +13,36 @@ function addInputListeners(fileInput, canvas) {
         ctx.drawImage(image, 0, 0);
 
         canvas.style.visibility = "visible";
+        updateOutput();
       });
     }
   });
+}
+
+const channels = "rgba";
+
+function getInputChannel(outputChannel) {
+  return document.querySelector(`input[name=${outputChannel}input]:checked`).value;
+}
+
+const outputCanvas = document.getElementById("output-canvas");
+
+function updateOutput() {
+  outputCanvas.width = redInputCanvas.width;
+  outputCanvas.height = redInputCanvas.height;
+
+  const outputData = getData(outputCanvas);
+  const redData = getData(redInputCanvas);
+
+  const inputChannel = getInputChannel("r");
+  const channelOffset = channels.indexOf(inputChannel);
+
+  packChannels(outputData.data, redData.data.subarray(channelOffset), redData.data.subarray(channelOffset), redData.data.subarray(channelOffset), redData.data.subarray(channelOffset));
+  outputCanvas.getContext("2d").putImageData(outputData, 0, 0);
+}
+
+function getData(canvas) {
+  return canvas.getContext("2d").getImageData(0, 0, canvas.width, canvas.height);
 }
 
 function packChannels(dst, srcR, srcG, srcB, srcA) {
